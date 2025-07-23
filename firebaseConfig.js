2ed40646718ff6cc5f7184f1aae0c4b5ca640aa1
hemangkrish007 <99663117+hemangkrish007@users.noreply.github.com>
2025-07-23 17:52:49 +0530

    remove hardcoded keys

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,16 +1,17 @@
+// firebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 // Firebase Config
-const firebaseConfig = {
-  apiKey: "AIzaSyC8XftSTgzzDGR45GsxSgPnCGeeQOrujyM",
-  authDomain: "omniplex-98c10.firebaseapp.com",
-  projectId: "omniplex-98c10",
-  storageBucket: "omniplex-98c10.firebasestorage.app",
-  messagingSenderId: "259049861013",
-  appId: "1:259049861013:web:6a9b65f57e15be4c3f909c",
-  measurementId: "G-G932NBHB2S"
+export const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
